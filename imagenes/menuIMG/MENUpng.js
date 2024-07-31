f769b0ef0c31589df390fce249f55465dac81b92
おSyllkom <154283359+Syllkom@users.noreply.github.com>
2024-07-31 07:55:07 -0500

    ㅤ

---
+++ imagenes/menuIMG/MENUpng.js
@@ -0,0 +1,33 @@
+const MENUpng = [
+   "https://telegra.ph/file/8224662c50f405787e703.jpg", //Imagen 1
+   "https://telegra.ph/file/d2a0da2cbb47ba237f3bc.jpg", //Imagen 2
+   "https://telegra.ph/file/3918e0645d74e72c153ab.jpg", //Imagen 3
+   "https://telegra.ph/file/8d010f335375bf3c5c4b7.jpg", //Imagen 4
+   "https://telegra.ph/file/cc55ea6fbe9bcff2d7974.jpg", //Imagen 5
+   "https://telegra.ph/file/b11c23b9e916465f3fa7f.jpg", //Imagen 6 
+   "https://telegra.ph/file/163a2b5da0881e344a83b.jpg", //Imagen 7
+   "https://telegra.ph/file/163a2b5da0881e344a83b.jpg", //Imagen 8
+   "https://telegra.ph/file/b22c794aef926c36b39e6.jpg", //Imagen 9
+   "https://telegra.ph/file/8b49657adf12ae36e6112.jpg", //Imagen 10
+   "https://telegra.ph/file/96cf607c72e665d6f6e9a.jpg", //Imagen 11
+   "https://telegra.ph/file/eeea641624b01fc11e672.jpg", //Imagen 12
+   "https://telegra.ph/file/aafd13b4bce744a74eb80.jpg", //Imagen 13
+   "https://telegra.ph/file/876423821cfe85067f538.jpg", //Imagen 14
+   "https://telegra.ph/file/c5363e0783875644e5c0a.jpg", //Imagen 15
+   "https://telegra.ph/file/6c5568c0386373beb1c5f.jpg", //Imagen 16
+   "https://telegra.ph/file/8c22d70248dd5e9f9011c.jpg", //Imagen 17
+   "https://telegra.ph/file/24e50f11cf4f29673e497.jpg", //Imagen 18
+   "https://telegra.ph/file/90609c31bb6da45b98d13.jpg", //Imagen 19
+   "https://telegra.ph/file/b30a8063a7a42dac14cdd.jpg", //Imagen 20
+   "https://telegra.ph/file/c9e07706bba1cb37fd564.jpg", //Imagen 21
+   "https://telegra.ph/file/c0d9a29dc12f6cd86fefe.jp", //Imagen 22
+   "https://telegra.ph/file/0dcf304b49eec3b9146fd.jpg" //Imagen 23
+];
+
+function getMENUpngLinks() {
+    return MENUpng;
+}
+
+module.exports = {
+    getMENUpngLinks
+};
